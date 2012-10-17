46f6ca1c0ef8e5d864ffcc6d27edf4b5250e786c
lueckert <lueckert@g149-lx21.htwg-konstanz.de>
2012-10-17 13:07:42 +0200

    arraydict update

--- src/dictionary/SortedArrayDictionary.java
+++ src/dictionary/SortedArrayDictionary.java
@@ -4,18 +4,71 @@ package dictionary;
  * SortedArrayDictionary implementiert ein Dictionary mit einem Feld, in dem die Datensätze
  * lückenlos und sortiert gespeichert werden. Für die Suche wird binäre Suche eingesetzt.
  */
-public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
+public class SortedArrayDictionary<K extends Comparable<? super K>, V> implements Dictionary<K, V> {
+	
+	private final static int INITIAL_CAPACITY = 10;
+	@SuppressWarnings("unchecked")
+	private Element<K,V>[] data = new Element[INITIAL_CAPACITY];
+	private int length;
+	
+	private static class Element<K, V>  {
+		K key;
+		V value;
+		public Element(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
 
 	@Override
 	public V insert(K key, V value) {
-		// TODO Auto-generated method stub
+		ensureCapacity();
+		int index;
+		
+		// Check if the key already exists
+		if ((index = getKeyIndex(key)) != -1) {
+			V oldval = data[index].value;
+			data[index].value = value;
+			return oldval;
+		}
+		
+		
+		
 		return null;
+	}
+	
+	private void ensureCapacity() {
+		
 	}
 
 	@Override
 	public V search(K key) {
-		// TODO Auto-generated method stub
-		return null;
+		int index = getKeyIndex(key);
+		if (index != -1) {
+			return data[index].value;
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Tries to retrieve the index of the internal element of the key. 
+	 * @return Array index of the key or -1 in case the key does not exist. 
+	 */
+	private int getKeyIndex(K key) {
+		int li = 0;
+		int re = this.length - 1;
+		while (li < re) {
+			int m = (li + re) / 2;
+			if (key.compareTo(data[m].key) < 0) {
+				re = m - 1;
+			} else if (key.compareTo(data[m].key) > 0) {
+				li = m + 1;
+			} else {
+				return m;
+			}
+		}
+		return -1;
 	}
 
 	@Override
