1775767d073a4e1736174b2541dfbaf820cc68a1
lueckert <lueckert@g149-lx21.htwg-konstanz.de>
2012-10-17 12:32:32 +0200

    created dictionary base classes

---
+++ src/dictionary/SortedArrayDictionary.java
@@ -0,0 +1,27 @@
+package dictionary;
+
+/**
+ * SortedArrayDictionary implementiert ein Dictionary mit einem Feld, in dem die Datensätze
+ * lückenlos und sortiert gespeichert werden. Für die Suche wird binäre Suche eingesetzt.
+ */
+public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
+
+	@Override
+	public V insert(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V search(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
