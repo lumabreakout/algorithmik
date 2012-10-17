8a79fd653ee47e058913f2f5cfa324ecd8ebc823
lueckert <lueckert@g149-lx21.htwg-konstanz.de>
2012-10-17 13:14:54 +0200

    pakete geändert

---
+++ src/aufgabe1/dictionary/TreeDictionary.java
@@ -0,0 +1,26 @@
+package aufgabe1.dictionary;
+
+/**
+ * TreeDictionary setzt die in der Vorlesung besprochenen AVL-Bäume ein.
+ */
+public class TreeDictionary<K, V> implements Dictionary<K, V> {
+
+	@Override
+	public V insert(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V search(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
