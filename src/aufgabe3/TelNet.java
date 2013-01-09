08ff42fece553cbe7c1353cd8c736e6c8437c284
OSI-Califronia <OSI-@gmx.net>
2013-01-09 13:51:51 +0100

    fertigg

--- src/aufgabe3/TelNet.java
+++ src/aufgabe3/TelNet.java
@@ -6,6 +6,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.JOptionPane;
+
 import aufgabe2.graph.AdjacencyListUndirectedGraph;
 import aufgabe2.graph.Edge;
 import aufgabe2.graph.UndirectedGraph;
@@ -14,6 +16,8 @@ public class TelNet {
 
 	private final int lbg;
 	private UndirectedGraph<TelKnoten> graph;
+	private Map<TelKnoten, TelKnoten> vorgaenger = new HashMap<TelKnoten, TelKnoten>();
+	private Map<TelKnoten, Double> distance = new HashMap<TelKnoten, Double>();
 
 	/**
 	 *   Legt ein neues Telefonnetz mit dem Leitungsbegrenzungswert lbg an.
@@ -23,8 +27,8 @@ public class TelNet {
 		this.graph = new AdjacencyListUndirectedGraph<TelKnoten>();
 	}
 
-	/**
-	 * Fügt einen neuen Telefonknoten mit Koordinate (x,y) dazu.
+	/*
+	 * Fuegt einen neuen Telefonknoten mit Koordinate (x,y) dazu.
 	 * und erstellt Verbindungen zu im Radius liegenden anderen Knoten.
 	 */
 	public void addTelKnoten(int x, int y) {
@@ -37,25 +41,67 @@ public class TelNet {
 	 * aufspannenden Baum mit dem Algorithmus von Prim.
 	 */
 	public void computeOptTelNet() {
-		
-		
-		
+		primAlgo(this.graph, graph.getVertexList().get(0));
+
+
 	}
-	
+
 	private void primAlgo(UndirectedGraph<TelKnoten> graph, TelKnoten startNode) {
 		List<TelKnoten> nodes = graph.getVertexList();
 		List<TelKnoten> kandidaten = new LinkedList<TelKnoten>();
 		List<TelKnoten> baum = new LinkedList<TelKnoten>();
-		Map<TelKnoten, TelKnoten> vorgaenger = new HashMap<TelKnoten, TelKnoten>();
-		
-		kandidaten.add(nodes.remove(0));
-		
+		vorgaenger.clear();
+		distance.clear();
+
+		for (TelKnoten v : nodes) {
+			vorgaenger.put(v, null);
+			distance.put(v, Double.POSITIVE_INFINITY);
+		}
+
+		distance.put(startNode, 0.0);
+		kandidaten.add(startNode);
+
 		while (!kandidaten.isEmpty()) {
-			// TODO:
+			TelKnoten v = removeNodeMinimalDistance(kandidaten, distance);
+			if (!baum.contains(v)) {
+				baum.add(v);
+			}
+
+			for (TelKnoten w : graph.getAdjacentVertexList(v)) {
+				if (!baum.contains(w)) {
+					if (Double.isInfinite(distance.get(w).doubleValue())) {
+						kandidaten.add(w);
+					}
+					Double cost = getCost(v, w);
+					if (cost.compareTo(distance.get(w)) < 0) {
+						vorgaenger.put(w, v);
+						distance.put(w, cost);
+					}
+				}
+			}
+		}
+
+		if (baum.size() != nodes.size()) {
+			System.out.println("Min-aufspannender Baum existiert nicht!");
 		}
 	}
-	
-	
+
+	private TelKnoten removeNodeMinimalDistance(List<TelKnoten> kandidaten, Map<TelKnoten, Double> distance) {
+
+		int minIndex = 0;
+		TelKnoten minNode = kandidaten.get(0);
+
+		for (int i = 0; i < kandidaten.size(); ++i) {
+			TelKnoten v = kandidaten.get(i);
+			if (distance.get(v).compareTo(distance.get(minNode)) < 0) {
+				minNode = v;
+				minIndex = i;
+			}
+		}
+
+		kandidaten.remove(minIndex);
+		return minNode;
+	}
 
 
 	private double getCost(TelKnoten u, TelKnoten v) {
@@ -65,34 +111,44 @@ public class TelNet {
 
 	/**
 	 * Zeichnet das gefundene optimale 
-	 * Telefonnetz mit der Größe xMax*yMax in ein Fenster.
+	 * Telefonnetz mit der Groesse xMax*yMax in ein Fenster.
 	 */
-	public void drawOptTelNet(int xMax, int yMax) {
-		StdDraw.setCanvasSize(xMax, yMax);
+	public void drawOptTelNet(int xMax, int yMax, boolean showAllEdges, double nodeSize) {
+		StdDraw.setCanvasSize(1000, 1000);
 		StdDraw.setXscale(0, xMax);
 		StdDraw.setYscale(0, yMax);
 		StdDraw.show(0);
-		
+
 		StdDraw.clear(Color.WHITE);
-		
-		// alle möglichen verbindungen einzeichnen
-		StdDraw.setPenColor(Color.GRAY);
-		for (Edge<TelKnoten> edge : this.graph.getEdgeList()) {
-			TelKnoten u = edge.getSource();
-			TelKnoten v = edge.getTarget();
-			StdDraw.line(u.x, u.y, u.x, v.y);
-			StdDraw.line(v.x, v.y, u.x, v.y);
+
+		// alle moelichen verbindungen einzeichnen
+		if (showAllEdges) {
+			StdDraw.setPenColor(Color.GRAY);
+			for (Edge<TelKnoten> edge : this.graph.getEdgeList()) {
+				TelKnoten u = edge.getSource();
+				TelKnoten v = edge.getTarget();
+				StdDraw.line(u.x, u.y, u.x, v.y);
+				StdDraw.line(v.x, v.y, u.x, v.y);
+			}
 		}
-		
-		// TODO: alle nötigen verbindungen einzeichnen
-		
-		
+
+		// alle verbindungen des min-aufspannenden graph zeichnen
+		StdDraw.setPenColor(Color.RED);
+		for (Edge<TelKnoten> edge : this.graph.getEdgeList()) {
+			TelKnoten u = edge.getTarget();
+			TelKnoten v = edge.getSource();
+			if (vorgaenger.get(u) == v || vorgaenger.get(v) == u) {
+				StdDraw.line(u.x, u.y, u.x, v.y);
+				StdDraw.line(v.x, v.y, u.x, v.y);
+			}
+		}
+
 		// alle telefonknoten einzeichnen
 		StdDraw.setPenColor(Color.BLACK);
 		for (TelKnoten k : this.graph.getVertexList()) {
-			StdDraw.filledCircle(k.x, k.y, 5);
+			StdDraw.filledCircle(k.x, k.y, nodeSize);
 		}
-	
+
 		StdDraw.show(0);
 	}
 
@@ -101,7 +157,7 @@ public class TelNet {
 	 * x-Koordinaten aus [0,xMax] und y-Koordinaten aus [0,yMax].
 	 */
 	public void generateRandomTelNet(int n, int xMax, int yMax) {
-		int gridsize = 20;
+		int gridsize = 10;
 		for (int i = 0; i < n; ++i) {
 			int x = (int) (Math.random() * xMax);
 			int y = (int) (Math.random() * yMax);
@@ -114,10 +170,10 @@ public class TelNet {
 				continue;
 			}
 		}
-		
+
 		generateEdges();
 	}
-	
+
 	public void generateEdges() {
 		this.graph.clearEdges();
 
@@ -145,15 +201,37 @@ public class TelNet {
 	}
 
 	/**
-	 * Liefert ein optimales Telefonnetz als Liste von Telefonverbindungen zurück.
+	 * Liefert ein optimales Telefonnetz als Liste von Telefonverbindungen zurueck.
 	 */
 	public List<TelVerbindung> 	getOptTelNet() {
-		// TODO:
-		return null;
+		List<TelVerbindung> list = new LinkedList<TelVerbindung>();
+		
+		List<TelKnoten> visited = new LinkedList<TelKnoten>();
+		
+		for (Map.Entry<TelKnoten, TelKnoten> entry : vorgaenger.entrySet()) {
+			TelKnoten v =  entry.getKey();
+			TelKnoten prev = entry.getValue();	
+			
+			if (prev == null) {
+				visited.add(v);
+				continue;
+			}
+			
+			while (prev != null && !visited.contains(v)) {
+				visited.add(v);
+				list.add(new TelVerbindung(v, prev));
+				
+				v = prev;
+				prev = vorgaenger.get(v);
+			}			
+			
+		}
+		
+		return list;
 	}
 
 	/**
-	 *  Liefert die Gesamtkosten eines optimalen Telefonnetzes zurück.
+	 *  Liefert die Gesamtkosten eines optimalen Telefonnetzes zurueck.
 	 */
 	public int 	getOptTelNetKosten() {
 		int sum = 0;
@@ -164,11 +242,39 @@ public class TelNet {
 	}
 
 	public static void 	main(java.lang.String[] args) {
-		int xmax = 500;
-		int ymax = 500;
-		TelNet net = new TelNet(100);
-		net.generateRandomTelNet(100, xmax, ymax);
-		net.drawOptTelNet(xmax, ymax);
-	
+
+//		aufgabe2();
+		aufgabe3();
 	}
+	
+	private static void aufgabe2() {
+		int xmax = 7;
+		int ymax = 7;
+		TelNet net = new TelNet(7);
+		net.addTelKnoten(1,1);
+		net.addTelKnoten(3, 1);
+		net.addTelKnoten(4,2);
+		net.addTelKnoten(3,4);
+		net.addTelKnoten(2,6);
+		net.addTelKnoten(4,7);
+		net.addTelKnoten(7,5);
+		net.generateEdges();
+		
+		net.computeOptTelNet();
+
+		net.drawOptTelNet(xmax, ymax, false, 0.1);
+		System.out.println("Kosten " + net.getOptTelNetKosten());
+	}
+	
+	private static void aufgabe3() {
+		int xmax = 1000;
+		int ymax = 1000;
+		TelNet net = new TelNet(100);
+		net.generateRandomTelNet(1000, xmax, ymax);
+		net.computeOptTelNet();
+
+		net.drawOptTelNet(xmax, ymax, false, 5);
+		System.out.println("Kosten " + net.getOptTelNetKosten());
+	}
+	
 }
